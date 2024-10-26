aa2293828fafce593a97edce2bc6cf0ac38ab1bb
agus <agustinolivieri30@gmail.com>
2024-10-26 15:28:24 -0300

    entidades Siguiendo, Usuario y Enofilo

---
+++ src/main/java/com/dsi/tp/bonvino/Models/Enofilo.java
@@ -0,0 +1,102 @@
+package com.dsi.tp.bonvino.Models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Enofilo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "imagen_perfil")
+    private String imagenPerfil;
+
+    @ManyToOne
+    @JoinColumn(name = "siguiendo_id")
+    private Siguiendo siguiendo;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "vino_id")
+    private Vino vino;
+
+    // Constructors
+    public Enofilo() {
+    }
+
+    // Getters y setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getImagenPerfil() {
+        return imagenPerfil;
+    }
+
+    public void setImagenPerfil(String imagenPerfil) {
+        this.imagenPerfil = imagenPerfil;
+    }
+
+    public Siguiendo getSiguiendo() {
+        return siguiendo;
+    }
+
+    public void setSiguiendo(Siguiendo siguiendo) {
+        this.siguiendo = siguiendo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Vino getVino() {
+        return vino;
+    }
+
+    public void setVino(Vino vino) {
+        this.vino = vino;
+    }
+
+    // Métodos
+    public String getNombreUsuario() {
+        return this.usuario.getNombre();
+    }
+
+    public boolean seguisABodega(Bodega bodega) {
+        return this.siguiendo.sosDeBodega(bodega);
+    }
+}
